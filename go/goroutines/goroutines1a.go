4cc067f9dd135e5627ae5812dddcaa9a73f8f2e2
Marcin Kik <mki1967@gmail.com>
2015-02-28 12:45:04 +0100

    Przykład z wieloma wątkami goroutines1a.go, oraz z kanałami goroutines3.go

---
+++ go/goroutines/goroutines1a.go
@@ -0,0 +1,27 @@
+/* przerobine z https://gobyexample.com/goroutines */
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+const maxNr = 4
+
+func f(from string, nr int) {
+	var interval time.Duration
+
+	for i := 0; i < 1000; i++ {
+		interval = time.Duration((100 + rand.Intn(3000)))
+		time.Sleep(time.Millisecond * interval)
+		fmt.Println(from, nr, ":", i)
+	}
+}
+
+func main() {
+	for i := 0; i < maxNr; i++ {
+		go f("Task", i)
+	}
+	var input string
+	fmt.Scanln(&input) // blokuje main()
+	fmt.Println("done")
+}
---
+++ go/goroutines/goroutines3.go
@@ -0,0 +1,42 @@
+/* przerobine z https://gobyexample.com/goroutines */
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+func w(from string, cout chan int) {
+	var interval time.Duration
+	var value int
+
+	for i := 0; i < 1000; i++ {
+		interval = time.Duration((100 + rand.Intn(3000)))
+		time.Sleep(time.Millisecond * interval)
+		value = rand.Intn(100)
+		fmt.Println(from, ": writing ", value)
+		cout <- value // zapis do kanału
+
+	}
+}
+
+func r(from string, cin chan int) {
+	var interval time.Duration
+	var value int
+
+	for i := 0; i < 1000; i++ {
+		interval = time.Duration((100 + rand.Intn(3000)))
+		time.Sleep(time.Millisecond * interval)
+		value = <-cin // odczyt z kanału
+		fmt.Println(from, ": read ", value)
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go w("Task1", ch)
+	go r("Task2", ch)
+
+	var input string
+	fmt.Scanln(&input) // blokuje main()
+	fmt.Println("done")
+}
