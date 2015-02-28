a2640a797a756a13b1a58a2c54a4c786be5c096c
Marcin Kik <mki1967@gmail.com>
2015-02-28 10:58:13 +0100

    Pierwszy przykład w Go

---
+++ go/goroutines/gorutines1.go
@@ -0,0 +1,28 @@
+/* przerobine z https://gobyexample.com/goroutines */
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+
+
+func f(from string) {
+	var interval time.Duration
+
+	for i := 0; i < 1000; i++ {
+                interval = time.Duration((100+rand.Intn(3000))) 
+		time.Sleep(  time.Millisecond * interval )
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	go f("Task1")
+	go f("Task2")
+	go f("Task3")
+
+        var input string
+	fmt.Scanln(&input) // blokuje main() 
+	fmt.Println("done")
+}
