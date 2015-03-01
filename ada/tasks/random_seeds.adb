b9a66c5421e4ae71a5591265f7a13dc502fd3c24
Marcin Kik <mki1967@gmail.com>
2015-03-01 16:13:51 +0100

    prostsza implementacja Random_Seeds

--- ada/tasks/random_seeds.adb
+++ ada/tasks/random_seeds.adb
@@ -2,15 +2,14 @@ with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
 
 package body Random_Seeds is
    function Make_Seeds(Size : Positive) return Seed_Array_Type is
-      GG : Generator ; -- global random generator
-      Array_Access : access  Seed_Array_Type;
+      GG : Generator ; -- random generator
+      Seed_Array :  Seed_Array_Type(1 .. Size);
    begin
       Reset(GG); -- Start the generator in a unique state in each run
-      Array_Access := new Seed_Array_Type(1 .. Size);
-      for I in Array_Access.all'Range loop
-	 Array_Access.all(I) := Integer( 1.0E6 * Random(GG)) ;
+      for I in Seed_Array'Range loop
+	 Seed_Array(I) := Integer( 1.0E6 * Random(GG)) ;
       end loop;
-      return Array_Access.all;
+      return Seed_Array;
    end;
 end Random_Seeds;
 
